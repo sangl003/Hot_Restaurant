cbc9e057a7ca79a39ecb9f2252b3d99eb5b79b1c
Nick Sedillos <nick.sedillos@gmail.com>
2018-09-29 10:59:05 -0500

    package.json

--- server.js
+++ server.js
@@ -15,10 +15,11 @@ app.use(express.json());
 // Create a few array variables that will hold the data
 var reservations = [];
 var waitlist = [];
+
 // Create a set of routes for getting and posting table data
 // /api/reservations
 // /api/waitlist
+
 // Create a set of routes for displaying the HTML pages
+
 // Use jQuery to run AJAX calls to GET and POST data from users to the Express server
-
-
