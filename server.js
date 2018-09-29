d1213783a53378da0f3375f6cfa3b0591729e39d
Git sangl003 <sharvari.sangle@gmail.com>
2018-09-29 11:18:41 -0500

    merged

--- server.js
+++ server.js
@@ -13,7 +13,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Create a few array variables that will hold the data
-var reservations = [];
+var reservations = [{
+    name: "Ryan",
+    phone: "545-899-4546",
+    email: "ryan@test.com",
+    uniqueid: "1"
+
+}];
 var waitlist = [];
 
 // Create a set of routes for getting and posting table data
@@ -31,18 +37,17 @@ app.get("/reservations", function(req, res) {
   });
 
 // /api/waitlist
-
+app.get("/waitlist", function(req, res) {
+    return res.json(waitlist);
+  });
 // Create a set of routes for displaying the HTML pages
 // /reserve
 // /tables
 
 // Use jQuery to run AJAX calls to GET and POST data from users to the Express server
-<<<<<<< HEAD
-=======
 
 // Starts the server to begin listening
 // =============================================================
 app.listen(PORT, function() {
 	console.log("App listening on PORT " + PORT);
   });
->>>>>>> 020da5b388dc9776f5da6faeea22ce6956ee37f2
