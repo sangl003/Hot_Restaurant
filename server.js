129d93a27176007b8bd338a4649ec1230312b645
Abdallah <10871403+AbdallahDay@users.noreply.github.com>
2018-09-29 11:11:11 -0500

    adding express routing

--- server.js
+++ server.js
@@ -17,9 +17,32 @@ var reservations = [];
 var waitlist = [];
 
 // Create a set of routes for getting and posting table data
+
 // /api/reservations
+app.get("/api/reservations", function(req, res) {
+    res.json(reservations);
+});
+
 // /api/waitlist
+app.get("/api/waitlist", function(req, res) {
+    res.json(waitlist);
+});
 
 // Create a set of routes for displaying the HTML pages
+
+// Home
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+// Reserve
+app.get("/reserve", function(req, res) {
+    res.sendFile(path.join(__dirname, "Reserve.html"));
+});
+
+// Tables
+app.get("/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "Tables.html"));
+});
 
 // Use jQuery to run AJAX calls to GET and POST data from users to the Express server
